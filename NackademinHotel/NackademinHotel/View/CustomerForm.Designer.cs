bb38ea7ff1e1edb3b401503ec67ed217ed93d0e5
Joakim Carlsson <Carlsson@DESKTOP-V8MGHQQ>
2020-11-04 15:42:03 +0100

    We can now create a new customer, edit, and delete a customer that does not have any bookings.

---
+++ NackademinHotel/NackademinHotel/View/CustomerForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿using System.ComponentModel;
+
+namespace NackademinHotel
+{
+    partial class CustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateOfBirthPicker = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.saveCustomerButton = new System.Windows.Forms.Button();
+            this.removeCustomerButton = new System.Windows.Forms.Button();
+            this.editCustomerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // customerListBox
+            // 
+            this.customerListBox.FormattingEnabled = true;
+            this.customerListBox.Location = new System.Drawing.Point(12, 35);
+            this.customerListBox.Name = "customerListBox";
+            this.customerListBox.Size = new System.Drawing.Size(304, 290);
+            this.customerListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kunder";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(442, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Förnamn:";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(512, 32);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(152, 20);
+            this.firstNameTextBox.TabIndex = 3;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(512, 58);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(152, 20);
+            this.lastNameTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(442, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Efternamn:";
+            // 
+            // dateOfBirthPicker
+            // 
+            this.dateOfBirthPicker.Location = new System.Drawing.Point(512, 89);
+            this.dateOfBirthPicker.Name = "dateOfBirthPicker";
+            this.dateOfBirthPicker.Size = new System.Drawing.Size(152, 20);
+            this.dateOfBirthPicker.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(442, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Födelseår:";
+            // 
+            // saveCustomerButton
+            // 
+            this.saveCustomerButton.Location = new System.Drawing.Point(442, 115);
+            this.saveCustomerButton.Name = "saveCustomerButton";
+            this.saveCustomerButton.Size = new System.Drawing.Size(92, 25);
+            this.saveCustomerButton.TabIndex = 8;
+            this.saveCustomerButton.Text = "Spara";
+            this.saveCustomerButton.UseVisualStyleBackColor = true;
+            this.saveCustomerButton.Click += new System.EventHandler(this.saveCustomerButton_Click);
+            // 
+            // removeCustomerButton
+            // 
+            this.removeCustomerButton.Location = new System.Drawing.Point(12, 331);
+            this.removeCustomerButton.Name = "removeCustomerButton";
+            this.removeCustomerButton.Size = new System.Drawing.Size(95, 27);
+            this.removeCustomerButton.TabIndex = 9;
+            this.removeCustomerButton.Text = "Ta Bort";
+            this.removeCustomerButton.UseVisualStyleBackColor = true;
+            this.removeCustomerButton.Click += new System.EventHandler(this.removeCustomerButton_Click);
+            // 
+            // editCustomerButton
+            // 
+            this.editCustomerButton.Location = new System.Drawing.Point(221, 331);
+            this.editCustomerButton.Name = "editCustomerButton";
+            this.editCustomerButton.Size = new System.Drawing.Size(95, 27);
+            this.editCustomerButton.TabIndex = 10;
+            this.editCustomerButton.Text = "Redigera";
+            this.editCustomerButton.UseVisualStyleBackColor = true;
+            this.editCustomerButton.Click += new System.EventHandler(this.editCustomerButton_Click);
+            // 
+            // CustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 450);
+            this.Controls.Add(this.editCustomerButton);
+            this.Controls.Add(this.removeCustomerButton);
+            this.Controls.Add(this.saveCustomerButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateOfBirthPicker);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.customerListBox);
+            this.Name = "CustomerForm";
+            this.Text = "CustomerForm";
+            this.Load += new System.EventHandler(this.CustomerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button editCustomerButton;
+
+        private System.Windows.Forms.Button removeCustomerButton;
+
+        private System.Windows.Forms.DateTimePicker dateOfBirthPicker;
+
+        private System.Windows.Forms.TextBox lastNameTextBox;
+
+        private System.Windows.Forms.Button saveCustomerButton;
+
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.ListBox customerListBox;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
