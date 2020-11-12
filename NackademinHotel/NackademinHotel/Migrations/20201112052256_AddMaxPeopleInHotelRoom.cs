936d44235446c6589bc0b0e8fe3162d24125b5e0
Joakim Carlsson <Carlsson@DESKTOP-V8MGHQQ>
2020-11-12 08:03:56 +0100

    When searching for hotelrooms. we now need too choose how many people that will stay in that room.

---
+++ NackademinHotel/NackademinHotel/Migrations/20201112052256_AddMaxPeopleInHotelRoom.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NackademinHotel.Migrations
+{
+    public partial class AddMaxPeopleInHotelRoom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxPeople",
+                table: "HotelRooms",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxPeople",
+                table: "HotelRooms");
+        }
+    }
+}
